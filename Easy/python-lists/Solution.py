41b93da61759f16888b394384126a8916ca2b274
Michał Malenda <michalmalenda@gmail.com>
2022-07-09 21:04:14 +0200

    up

--- Easy/python-lists/Solution.py
+++ Easy/python-lists/Solution.py
@@ -3,19 +3,19 @@ def decisionControl(commandList):
     for y in range(len(commandList)):
         if (commandList[y]).startswith('insert'):
             vals = commandList[y].split(" ")
-            vals.remove('insert')
+            out.insert(int(vals[1]),int(vals[2]))
         elif (commandList[y]).startswith('print'):
             print(out)
         elif (commandList[y]).startswith('remove'):
             vals = commandList[y].split(" ")
-            out.remove()
+            out.remove(int(vals[1]))
         elif (commandList[y]).startswith('append'):
             vals = commandList[y].split(" ")
-            out.append(vals[len(vals)])
+            out.append(int(vals[len(vals)-1]))
         elif (commandList[y]).startswith('sort'):
             out.sort()
         elif (commandList[y]).startswith('pop'):
-            out.pop(len(out))
+            out.pop(len(out)-1)
         elif (commandList[y]).startswith('reverse'):
             out.reverse()
 
